bd27958bf51a326d78cc7199f7af6b05e223e273
Ben Strobel <benedikt.strobel@tum.de>
2025-05-08 17:51:49 +0200

    Use web-llm instead of ollama

--- lib/game.ts
+++ lib/game.ts
@@ -1,132 +1,192 @@
+import { MLCEngine, ResponseFormat } from "@mlc-ai/web-llm";
+
 interface Soldier {
-    id: number;
-    team: "red" | "blue";
-    x: number;
-    y: number;
-    health: number;
+  id: number;
+  team: "red" | "blue";
+  x: number;
+  y: number;
+  health: number;
 }
 
 interface GameState {
-    currentCommandRed: string;
-    currentCommandBlue: string;
-    soldiers: {[id: number]: Soldier};
+  currentCommandRed: string;
+  currentCommandBlue: string;
+  soldiers: { [id: number]: Soldier };
 }
 
 interface OllamaResponse {
-    model: string;
-    created_at: string;
-    response: GameResponse
+  model: string;
+  created_at: string;
+  response: GameResponse;
 }
 
 interface GameResponse {
-    reasoning: string;
-    command: string;
+  reasoning: string;
+  command: string;
 }
 
-type GameCommand = ({action: "move", x: number, y: number} | {action: "attack", x: number, y: number} | {action: "nothing"}) & {reason: string};
+type GameCommand = (
+  | { action: "move"; x: number; y: number }
+  | { action: "attack"; x: number; y: number }
+  | { action: "nothing" }
+) & { reason: string };
 
 const ollamaRequestFormatBase = {
-    type: "object",
-    properties: {
-      reasoning: {
-        type: "string"
-      },
-      command: {
-        type: "string",
-        enum: ["nothing"]
-      }
+  type: "object",
+  properties: {
+    reasoning: {
+      type: "string",
     },
-    required: [
-      "reasoning",
-      "command"
-    ]
+    command: {
+      type: "string",
+      enum: ["nothing"],
+    },
+  },
+  required: ["reasoning", "command"],
+};
+const boardSize = 10;
+
+export async function loop(engine: MLCEngine) {
+  let state: GameState = {
+    currentCommandRed: "Flank and then attack the enemy soldier.",
+    currentCommandBlue: "Finish off the wounded soldier.",
+    soldiers: {
+      1: { id: 1, team: "red", x: 0, y: 0, health: 100 },
+      2: { id: 2, team: "red", x: 1, y: 0, health: 25 },
+      3: { id: 3, team: "blue", x: 5, y: 5, health: 100 },
+    },
+  };
+
+  console.log("Starting loop...");
+  while (true) {
+    for (const soldierId of Object.keys(state.soldiers)) {
+      let command = await makeRequestForSoldier(
+        parseInt(soldierId),
+        state,
+        engine
+      );
+      console.log(
+        `Turn of soldier: ${soldierId} | Command: ${JSON.stringify(command)}`
+      );
+      updateGameState(state, parseInt(soldierId), command);
+      // console.log(`New game state: ${JSON.stringify(state)}`);
+      printGameState(state);
+    }
   }
-  const boardSize = 10;
-
-export async function loop() {
-    let state: GameState = {
-        currentCommandRed: "Flank and then attack the enemy soldier.",
-        currentCommandBlue: "Finish off the wounded soldier.",
-        soldiers: {
-            1: { id: 1, team: "red", x: 0, y: 0, health: 100 },
-            2: { id: 2, team: "red", x: 1, y: 0, health: 25 },
-            3: { id: 3, team: "blue", x: 5, y: 5, health: 100 },
-        }
-    }
-
-    console.log("Starting loop...")
-    while(true) {
-        for(const soldierId of Object.keys(state.soldiers)) {
-            let command = await makeRequestForSoldier(parseInt(soldierId), state);
-            console.log(`Turn of soldier: ${soldierId} | Command: ${JSON.stringify(command)}`);
-            updateGameState(state, parseInt(soldierId), command);
-            // console.log(`New game state: ${JSON.stringify(state)}`);
-            printGameState(state);
-        }
-    }
 }
 
 function printGameState(state: GameState) {
-    let r = "";
-    for(let y = 0; y < boardSize; y++) {
-        for(let x = 0; x < boardSize; x++) {
-            let soldier = Object.values(state.soldiers).find(s => s.x === x && s.y === y);
-            if(soldier) {
-                r += soldier.team === "red" ? "R" : "B";
-            } else {
-                r += " ";
-            }
-        }
-        r += "\n";
+  let r = "";
+  for (let y = 0; y < boardSize; y++) {
+    for (let x = 0; x < boardSize; x++) {
+      let soldier = Object.values(state.soldiers).find(
+        (s) => s.x === x && s.y === y
+      );
+      if (soldier) {
+        r += soldier.team === "red" ? "R" : "B";
+      } else {
+        r += " ";
+      }
     }
-    console.log(r);
+    r += "\n";
+  }
+  console.log(r);
 }
 
-function parseCoordinates(coords: string): {x: number, y: number} {
-    let [x, y] = coords.replaceAll("[", "").replaceAll("]", "").split(",").map(Number);
-    return {x, y};
+function parseCoordinates(coords: string): { x: number; y: number } {
+  let [x, y] = coords
+    .replaceAll("[", "")
+    .replaceAll("]", "")
+    .split(",")
+    .map(Number);
+  return { x, y };
 }
 
-function updateGameState(state: GameState, soldierId: number, command: GameCommand) {
-    if(command.action === "nothing") return;
-    if(command.action === "attack") {
-        let targetSoldier = Object.values(state.soldiers).find(s => s.x === command.x && s.y === command.y);
-        if(targetSoldier) {
-            targetSoldier.health -= 50;
-            // Have to change soldier loop for this
-            /* if(targetSoldier.health <= 0) {
+function updateGameState(
+  state: GameState,
+  soldierId: number,
+  command: GameCommand
+) {
+  if (command.action === "nothing") return;
+  if (command.action === "attack") {
+    let targetSoldier = Object.values(state.soldiers).find(
+      (s) => s.x === command.x && s.y === command.y
+    );
+    if (targetSoldier) {
+      targetSoldier.health -= 50;
+      // Have to change soldier loop for this
+      /* if(targetSoldier.health <= 0) {
                 delete state.soldiers[targetSoldier.id];
             } */
-        }
-    } else if (command.action === "move") {
-        let soldier = state.soldiers[soldierId];
-        let blockingSoldier = Object.values(state.soldiers).find(s => s.x === command.x && s.y === command.y);
-        if(blockingSoldier) return;
-        if(command.x < 0 || command.x > boardSize-1 || command.y < 0 || command.y > boardSize-1) return;
-        soldier.x = command.x;
-        soldier.y = command.y;
     }
+  } else if (command.action === "move") {
+    let soldier = state.soldiers[soldierId];
+    let blockingSoldier = Object.values(state.soldiers).find(
+      (s) => s.x === command.x && s.y === command.y
+    );
+    if (blockingSoldier) return;
+    if (
+      command.x < 0 ||
+      command.x > boardSize - 1 ||
+      command.y < 0 ||
+      command.y > boardSize - 1
+    )
+      return;
+    soldier.x = command.x;
+    soldier.y = command.y;
+  }
 }
 
-async function makeRequestForSoldier(soldierId: number, state: GameState): Promise<GameCommand> {
-    let prompt = getPromptForSoldier(soldierId, state);
-    let format = getFormatForSoldier(soldierId, state);
-    let response = await ollamaRequest("gemma3:1b", prompt, format);
-    if (response.command.startsWith("moveto")) {
-        let coords = parseCoordinates(response.command.replace("moveto", ""));
-        return {action: "move", reason: response.reasoning, x: coords.x, y: coords.y};
-    } else if (response.command.startsWith("attack")) {
-        let coords = parseCoordinates(response.command.replace("attack", ""));
-        return {action: "move", reason: response.reasoning, x: coords.x, y: coords.y};
-    } else {
-        return {action: "nothing", reason: response.reasoning};
-    }
+async function makeRequestForSoldier(
+  soldierId: number,
+  state: GameState,
+  engine: MLCEngine
+): Promise<GameCommand> {
+  let prompt = getPromptForSoldier(soldierId, state);
+  let schema = getFormatForSoldier(soldierId, state);
+  let response = JSON.parse(
+    (
+      await engine.chatCompletion({
+        messages: [
+          { role: "system", content: prompt.system },
+          { role: "user", content: prompt.user },
+        ],
+        response_format: {
+          type: "json_object",
+          schema: JSON.stringify(schema),
+        } as ResponseFormat,
+      })
+    ).choices[0].message.content!
+  ) as GameResponse;
+  if (response.command.startsWith("moveto")) {
+    let coords = parseCoordinates(response.command.replace("moveto", ""));
+    return {
+      action: "move",
+      reason: response.reasoning,
+      x: coords.x,
+      y: coords.y,
+    };
+  } else if (response.command.startsWith("attack")) {
+    let coords = parseCoordinates(response.command.replace("attack", ""));
+    return {
+      action: "move",
+      reason: response.reasoning,
+      x: coords.x,
+      y: coords.y,
+    };
+  } else {
+    return { action: "nothing", reason: response.reasoning };
+  }
 }
 
-function getPromptForSoldier(soldierId: number, state: GameState): string {
-    let s = state.soldiers[soldierId];
-    // You can only do one action for now, but you will be asked to do another action after all other soldiers have made their turn.
-    return `You are a soldier in a game.
+function getPromptForSoldier(
+  soldierId: number,
+  state: GameState
+): { system: string; user: string } {
+  let s = state.soldiers[soldierId];
+  // You can only do one action for now, but you will be asked to do another action after all other soldiers have made their turn.
+  return {
+    system: `You are a soldier in a game.
     You can do one of the following actions per turn: move to an adjacent field, attack an adjacent field, do nothing.
     If the coordinate you want to move to, is blocked by another soldier, you cannot move there.
     You can attack a field when its directly adjacent to you.
@@ -134,42 +194,94 @@ function getPromptForSoldier(soldierId: number, state: GameState): string {
 
     You are soldier ${s.id}.
     This is a list of all soldiers:
-    [${Object.values(state.soldiers).map(s => `{id:'${s.id}', team:'${s.team}', coords: [${s.x},${s.y}], health:'${s.health}'}`).join("\n")}]
-
-    This is the command you received from your commander: ${s.team === "red" ? state.currentCommandRed : state.currentCommandBlue}
-
-    What will you do this turn?
-    Respond using JSON`;
+    [${Object.values(state.soldiers)
+      .map(
+        (s) =>
+          `{id:'${s.id}', team:'${s.team}', coords: [${s.x},${s.y}], health:'${s.health}'}`
+      )
+      .join("\n")}]
+    Respond using JSON
+    You will be now receiving your command for this turn from your commander:
+    `,
+    user: s.team === "red" ? state.currentCommandRed : state.currentCommandBlue,
+  };
 }
 
 function getFormatForSoldier(soldierId: number, state: GameState): any {
-    let format = structuredClone(ollamaRequestFormatBase);
-    let s = state.soldiers[soldierId];
-    let e = ["nothing"];
-    if(Object.values(state.soldiers).find(o => o.x === s.x+1 && o.y === s.y) === undefined) e.push(`moveto[${s.x+1},${s.y}]`);
-    if(Object.values(state.soldiers).find(o => o.x === s.x-1 && o.y === s.y) === undefined) e.push(`moveto[${s.x-1},${s.y}]`);
-    if(Object.values(state.soldiers).find(o => o.x === s.x && o.y === s.y+1) === undefined) e.push(`moveto[${s.x},${s.y+1}]`);
-    if(Object.values(state.soldiers).find(o => o.x === s.x && o.y === s.y-1) === undefined) e.push(`moveto[${s.x},${s.y-1}]`);
+  let format = structuredClone(ollamaRequestFormatBase);
+  let s = state.soldiers[soldierId];
+  let e = ["nothing"];
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x + 1 && o.y === s.y
+    ) === undefined
+  )
+    e.push(`moveto[${s.x + 1},${s.y}]`);
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x - 1 && o.y === s.y
+    ) === undefined
+  )
+    e.push(`moveto[${s.x - 1},${s.y}]`);
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x && o.y === s.y + 1
+    ) === undefined
+  )
+    e.push(`moveto[${s.x},${s.y + 1}]`);
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x && o.y === s.y - 1
+    ) === undefined
+  )
+    e.push(`moveto[${s.x},${s.y - 1}]`);
 
-    if(Object.values(state.soldiers).find(o => o.x === s.x+1 && o.y === s.y) !== undefined) e.push(`attack[${s.x+1},${s.y}]`);
-    if(Object.values(state.soldiers).find(o => o.x === s.x-1 && o.y === s.y) !== undefined) e.push(`attack[${s.x-1},${s.y}]`);
-    if(Object.values(state.soldiers).find(o => o.x === s.x && o.y === s.y+1) !== undefined) e.push(`attack[${s.x},${s.y+1}]`);
-    if(Object.values(state.soldiers).find(o => o.x === s.x && o.y === s.y-1) !== undefined) e.push(`attack[${s.x},${s.y-1}]`);
-    format.properties.command.enum = e;
-    return format;
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x + 1 && o.y === s.y
+    ) !== undefined
+  )
+    e.push(`attack[${s.x + 1},${s.y}]`);
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x - 1 && o.y === s.y
+    ) !== undefined
+  )
+    e.push(`attack[${s.x - 1},${s.y}]`);
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x && o.y === s.y + 1
+    ) !== undefined
+  )
+    e.push(`attack[${s.x},${s.y + 1}]`);
+  if (
+    Object.values(state.soldiers).find(
+      (o) => o.x === s.x && o.y === s.y - 1
+    ) !== undefined
+  )
+    e.push(`attack[${s.x},${s.y - 1}]`);
+  format.properties.command.enum = e;
+  return format;
 }
 
-async function ollamaRequest(model: string, prompt: string, format: any): Promise<GameResponse> {
-    return fetch("http://localhost:11434/api/generate", {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            model: model,
-            prompt: prompt,
-            format: format,
-            stream: false
-        }),
-    }).then(async (response) => JSON.parse((await response.json()).response) as GameResponse);
+async function ollamaRequest(
+  model: string,
+  prompt: string,
+  format: any
+): Promise<GameResponse> {
+  return fetch("http://localhost:11434/api/generate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: model,
+      prompt: prompt,
+      format: format,
+      stream: false,
+    }),
+  }).then(
+    async (response) =>
+      JSON.parse((await response.json()).response) as GameResponse
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,36 @@
-import { Button, Group } from "@mantine/core";
-import { useEffect } from "react";
+import { Button, Group, Loader, Text } from "@mantine/core";
+import { useEffect, useMemo, useState } from "react";
 import { loop } from "../lib/game";
+import { CreateMLCEngine, MLCEngine } from "@mlc-ai/web-llm";
 
 export default function IndexPage() {
+  const [loading, setLoading] = useState(0);
+  const [engine, setEngine] = useState<MLCEngine | null>(null);
 
   useEffect(() => {
-    loop();
-  }, [])
+    CreateMLCEngine("Llama-3.2-1B-Instruct-q4f16_1-MLC", {
+      initProgressCallback: (report) => setLoading(report.progress),
+    })
+      .then((x) => {
+        setEngine(x);
+      })
+      .catch((x) => console.error(x));
+  }, []);
+
+  useEffect(() => {
+    if (loading === 1) {
+      loop(engine!);
+    }
+  }, [loading]);
 
   return (
     <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
+      {loading < 1 && (
+        <Group>
+          <Loader />
+        </Group>
+      )}
+      {loading === 1 && <Button size="xl">Game running!</Button>}
     </Group>
   );
 }
